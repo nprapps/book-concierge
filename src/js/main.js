7bf879a9e7a08c3f147e584a20239b7d3d18db72
Thomas Wilburn <TWilburn@ad.npr.org>
2019-08-26 10:12:00 -0500

    Safeguard against async year double-loading

--- src/js/main.js
+++ src/js/main.js
@@ -11,7 +11,7 @@ TODO
 var $ = require("./lib/qsa");
 var debounce = require("./lib/debounce");
 
-var yearsLoaded = new Set();
+var yearsLoaded = {};
 var books = [];
 
 var controls = $.one("form.filters");
@@ -59,59 +59,67 @@ var processFilters = async function() {
 };
 
 var getBooks = async function(years) {
-  var pending = years.filter(y => !yearsLoaded.has(y)).map(async function(y) {
-    var response = await fetch(`./${y}.json`);
-    var data = await response.json();
+  var pending = years.map(function(y) {
+    if (yearsLoaded[y]) {
+      return yearsLoaded[y].then(() => []);
+    };
 
-    // clear out placeholders
-    $(".placeholder", container).forEach(e => e.parentElement.removeChild(e));
+    yearsLoaded[y] = new Promise(async function(ok, fail) {
 
-    yearsLoaded.add(y);
+      var response = await fetch(`./${y}.json`);
+      var data = await response.json();
 
-    // process the data
-    data.forEach(function(book) {
-      book.tags = new Set(book.tags.split(/\|\s/g).map(t => t.trim()));
-      var element = document.createElement("div");
-      element.className = "book-container";
-      var aspect = "";
-      if (book.dimensions.height) {
-        aspect = `padding-bottom: ${book.dimensions.height / book.dimensions.width * 100}%`;
+      // clear out placeholders
+      $(".placeholder", container).forEach(e => e.parentElement.removeChild(e));
+
+      // process the data
+      data.forEach(function(book) {
+        book.tags = new Set(book.tags.split(/\|\s/g).map(t => t.trim()));
+        var element = document.createElement("div");
+        element.className = "book-container";
+        var aspect = "";
+        if (book.dimensions.height) {
+          aspect = `padding-bottom: ${book.dimensions.height / book.dimensions.width * 100}%`;
+        }
+        element.innerHTML = `
+    <div class="cover-container" style="${aspect}">
+      <img 
+        ${nativeLazy ? "src" : "data-src"}="./assets/covers/${book.isbn}.jpg"
+        class="cover"
+        alt="${book.title}"
+        decoding="async"
+        loading="lazy"
+        width="${book.dimensions.width}"
+        height="${book.dimensions.height}"
+        intrinsicsize="${book.dimensions.width}x${book.dimensions.height}"
+      >
+      </div>
+    <div class="hover-data">
+      <div class="cover-text">
+        <b>${book.title}</b>
+        <br>by ${book.author}
+      </div>
+
+      <div class="read-more">
+        Read more &raquo;
+      </div>
+    </div>
+        `;
+        book.element = element;
+        container.appendChild(element);
+      });
+
+      //reset lazy-loading images
+      if (!nativeLazy) {
+        lazyImages = $("[data-src]");
+        onScroll();
       }
-      element.innerHTML = `
-  <div class="cover-container" style="${aspect}">
-    <img 
-      ${nativeLazy ? "src" : "data-src"}="./assets/covers/${book.isbn}.jpg"
-      class="cover"
-      alt="${book.title}"
-      decoding="async"
-      loading="lazy"
-      width="${book.dimensions.width}"
-      height="${book.dimensions.height}"
-      intrinsicsize="${book.dimensions.width}x${book.dimensions.height}"
-    >
-    </div>
-  <div class="hover-data">
-    <div class="cover-text">
-      <b>${book.title}</b>
-      <br>by ${book.author}
-    </div>
 
-    <div class="read-more">
-      Read more &raquo;
-    </div>
-  </div>
-      `;
-      book.element = element;
-      container.appendChild(element);
+      ok(data);
     });
 
-    //reset lazy-loading images
-    if (!nativeLazy) {
-      lazyImages = $("[data-src]");
-      onScroll();
-    }
+    return yearsLoaded[y];
 
-    return data;
   });
   
   var nested = await Promise.all(pending);
