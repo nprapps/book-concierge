3b70fa0c6e9add6a97135d383f48d5e99def2678
thomaswilburn <twilburn@npr.org>
2021-08-18 11:51:51 -0500

    Download covers

--- tasks/covers.js
+++ tasks/covers.js
@@ -12,43 +12,22 @@ module.exports = function(grunt) {
   };
 
   // grunt doesn't like top-level async functions
-  var getCovers = async function(books) {
+  var getCovers = async function(books, year) {
 
     var limit = 10;
+
+    grunt.file.mkdir(`temp/${year}`)
 
     for (var i = 0; i < books.length; i += limit) {
       console.log(`Requesting books ${i}-${i + limit}`);
       var batch = books.slice(i, i + limit);
       var requests = batch.map(async function(book) {
-        var { isbn, seamus } = book;
-        if (!isbn) return;
-        var path = `src/assets/covers/${isbn}.jpg`
+        var { image, cover } = book;
+        if (!image) return;
+        var path = `temp/${year}/${cover}.jpg`
         if (grunt.file.exists(path)) return true;
-        var params = {
-          Value: isbn,
-          UserID: process.env.BAKER_TAYLOR_API_USERID,
-          Password: process.env.BAKER_TAYLOR_API_PASSWORD,
-          Return: "T",
-          Type: "L"
-        }
-        var response = await fetch(getEndpoint(params));
+        var response = await fetch(image);
         var contents = await response.buffer();
-        // if it's too small, let's get it from Seamus instead
-        if (contents.length < 5000) {
-          console.log(`Baker & Taylor bailed on ${isbn}, pulling Seamus image...`);
-          response = await fetch(`https://npr.org/books/titles/${seamus}`);
-          var page = await response.text();
-          var $ = cheerio.load(page);
-          var tag = $(".bucketwrap.book .img, .bucketwrap.bookedition .img");
-          if (!tag.length) {
-            console.log(`Unable to load an image from Seamus for "${book.title}" (${book.year})`);
-            return;
-          }
-          var src = tag[0].attribs["data-original"] || tag[0].attribs.src;
-          src = src.replace(/-s\d+.*\.jpg/, "-s400-c70.jpg");
-          var image = await fetch(src);
-          contents = await image.buffer();
-        }
         await fs.writeFile(path, contents);
         await wait(1000);
       });
@@ -66,7 +45,11 @@ module.exports = function(grunt) {
 
     // get all books from all sheets
     var books = grunt.data.shelf;
-    getCovers(books).then(done);
+    var year = grunt.option("year");
+    if (!year) {
+      grunt.fail.fatal("Please specify --year");
+    }
+    getCovers(books, year).then(done);
     
   });
 };
